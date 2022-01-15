a664c99c41058b80e13fe4f28b3d57770e976b66
KierenB <4819592+KierenB@users.noreply.github.com>
2022-01-14 16:54:36 -0800

    Add env variables
    Deployed to Rinkeby successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+//Importing and configuring our .env file that we use to securely store our environment variables
+require("dotenv").config();
+
+// Some quick checks to make sure our .env is working.
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY == "") {
+  console.log("🛑 Private key not found.")
+}
+
+if (!process.env.STAGING_ALCHEMY_API_URL || process.env.STAGING_ALCHEMY_API_URL == "") {
+  console.log("🛑 Alchemy API URL not found.")
+}
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -21,6 +33,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
