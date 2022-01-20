2a966bb690dc802ed9afaa461d036ad1245c8a3f
KierenB <4819592+KierenB@users.noreply.github.com>
2022-01-20 11:17:40 -0800

    Update to push messages on chain

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,9 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
   
     console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
   
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
@@ -13,19 +11,20 @@ const main = async () => {
     let waveValue;
     waveValue = await waveContract.getTotalValue();
   
-    let waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
-  
+
     waveCount = await waveContract.getTotalWaves();
 
     waveValue = await waveContract.getTotalValue();
-  
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
 
-    waveValue = await waveContract.getTotalValue();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  
   };
   
   const runMain = async () => {
